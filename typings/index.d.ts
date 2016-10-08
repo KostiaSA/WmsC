7b4be70537589090ed16e57f26c3a787a8b1cd9c
KostiaSA <savchenkov@buhta.ru>
2016-10-08 20:57:44 +0300

    Initial commit

---
+++ typings/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="globals/chalk/index.d.ts" />
+/// <reference path="globals/lodash/index.d.ts" />
+/// <reference path="globals/mssql/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
